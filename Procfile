2e875d595236338fc286854b8d54da477e8f6398
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2016-02-13 23:46:17 -0500

    Dj Cling. New Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn temba.wsgi --log-file -
-
+worker: python manage.py celeryd --beat
--- temba/wsgi.py
+++ temba/wsgi.py
@@ -11,7 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "temba.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling, MediaCling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(MediaCling(get_wsgi_application()))
